665ea7bf4cf059eba77dc2b125cc3166b5a884ad
AiwendilCiunio <ciunio@gmail.com>
2025-10-30 22:51:50 +0100

    added env variable for websocket url

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -2,16 +2,18 @@ import { inject, Injectable, signal } from "@angular/core";
 import { MessageApiService } from "../api/message-api.service";
 import { EventMessage } from "../model/event-message.model";
 import { Client } from "@stomp/stompjs";
+import { environment } from "../../environments/environment";
 
 @Injectable({ providedIn: 'root' })
 export class MessageService {
 
     messages = signal<EventMessage[]>([]);
     private client: Client;
+    private brokerUrl = `${environment.brokerUrl}`
 
     constructor() {
         this.client = new Client({
-            brokerURL: 'ws://localhost:8080/ws',
+            brokerURL: this.brokerUrl,
             reconnectDelay: 5000
         });
         this.client.onConnect = () => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://lionfish-app-4ojnf.ondigitalocean.app/'
+    baseUrl: 'https://lionfish-app-4ojnf.ondigitalocean.app',
+    brokerUrl: 'wss://lionfish-app-4ojnf.ondigitalocean.app/ws'
 };
